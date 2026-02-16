6e240bb0a31491b28d844324ff5b9b3708b8a4be
DamienLOUPIAS <loupias.damien@gmail.com>
2026-02-16 17:34:04 +0100

    Update scriptFormulaire.js

--- js/scriptFormulaire.js
+++ js/scriptFormulaire.js
@@ -180,13 +180,6 @@ document.addEventListener("DOMContentLoaded", () => {
 	window.addEventListener("resize", resizeCanvas);
 	resizeCanvas();
 
-
-	ctx.fillStyle = "red"; // STUB
-
-	ctx.fillRect(500, 500, 150, 100);
-
-	ctx.clearRect(0, 0, canvas.width, canvas.height);
-
 	
 	class Leaf {
 		constructor() {
@@ -195,33 +188,42 @@ document.addEventListener("DOMContentLoaded", () => {
 
 		reset() {
 			this.x = Math.random() * canvas.width;
+			this.Vx = 0;
 			this.y = -500 * Math.random();
-			this.size = 20 + Math.random() * 20;
-			this.speedY = 1 + Math.random() * 2;
+			this.Vy = 0;
+			this.size = 10 + Math.random() * 10;
 			this.angle = Math.random() * Math.PI * 2;
-			this.rotationSpeed = (Math.random() - 0.5) * 0.05;
-			this.swing = Math.random() * 2 + 1;
-			this.swingSpeed = Math.random() * 0.02 + 0.01;
+			this.t = Math.random();
+			this.vDescente = 0.05 + Math.random() * 0.1;
+			this.largeur = 10.0 * Math.random() + 3;
+			this.depHorizontal = 0.0;
+			this.alpha = -1.0;
 		}
 
 		update() {
-			this.y += this.speedY;
-			this.angle += this.rotationSpeed;
-			this.x += Math.sin(this.y * this.swingSpeed) * this.swing;
-
-			// if (this.y > canvas.height + 50) {
-			// 	this.reset();
+			this.Vy = this.vDescente * this.t + this.alpha * Math.cos(2 * this.t);
+			this.Vx = 0.5 * this.largeur * Math.sin(this.t) + this.depHorizontal * 1 * this.t;
+			this.y += this.Vy;
+			this.x += this.Vx;
+			this.xPrime = (this.depHorizontal + this.largeur * Math.cos(this.t))
+			this.angle = 0.9 * this.angle + 0.1 * ((this.vDescente - 2 * this.alpha * Math.sin(2 * this.t)) / this.xPrime );
+			// this.angle = (this.vDescente - 2 * this.alpha * Math.sin(2 * this.t)) / this.xPrime;
+			// this.angle = (Math.PI * Math.cos(this.x / Math.sqrt(this.x * this.x + this.y * this.y))) / 180;
+			// if (this.Vx < -0.025 &&  0.025 < this.Vx) {
+			// 	this.angle = Math.tan(this.Vy/this.Vx);
 			// }
+			this.t += 0.04;
 		}
 
 		draw() {
 			ctx.save();
 			ctx.translate(this.x, this.y);
-			ctx.rotate(this.angle);
+			ctx.rotate(this.angle + Math.PI / 2);
 
 			ctx.fillStyle = "#e980a1";
 			ctx.beginPath();
 			ctx.ellipse(0, 0, this.size * 0.6, this.size, 0, 0, Math.PI * 2);
+			console.log(this.xPrime);
 			ctx.fill();
 
 			ctx.restore();
@@ -240,7 +242,7 @@ document.addEventListener("DOMContentLoaded", () => {
   	});
 
 	const leaves = [];
-	for (let i = 0; i < 15; i++) {
+	for (let i = 0; i < 30; i++) {
 		leaves.push(new Leaf());
 	}
 
@@ -252,10 +254,13 @@ document.addEventListener("DOMContentLoaded", () => {
 			leaf.draw();
 		});
 
-		requestAnimationFrame(animate);
+
+		requestAnimationFrame(animate); // performance.now
 	}
 
 	animate();
 
+
 });
+
 
